157c822d23d566e6892c59ab729b9169526a7091
Viktoria <v.sapunkova@neurobotics.ru>
2020-03-19 16:00:16 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/PostVk.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+public class PostVk {
+    private String namePost;
+    private String date;
+    private String imageUrl;
+    private String info;
+    private String id;
+    private String commentsInfo;
+    private int commentsQuantity;
+    private int views;
+    private int reposts;
+    private int likes;
+
+}
